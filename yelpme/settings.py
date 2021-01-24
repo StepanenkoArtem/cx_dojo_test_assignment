045e46227a17edcfb13dd46b3c8ccd46c0ada154
Artem Stepanenko <artem@stepanenko.one>
2021-01-24 22:53:02 +0200

    add loading settings from enviroment variables incuding dotenv file

--- yelpme/settings.py
+++ yelpme/settings.py
@@ -1,18 +1,37 @@
 # coding=utf-8
 """Yelpme settings."""
 
-# Default numbers of rows per request getting from yelp.com
-DEFAULT_ROWS_NUM = 50
+import os
 
-# Necessary business data
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# YELP API Settings
+DEFAULT_ROWS_NUM = 50
+YELP_API_URL = 'https://api.yelp.com/v3/businesses/search'
+YELP_API_KEY = os.environ.get('YELPME_YELPAPI_KEY')
+
+# Coogle Places API Settings
+GMP_API_KEY = os.environ.get('YELPME_GMPAPI_KEY')
+GMP_ID_API_URL = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json'
+GMP_DETAILS_API_URL = 'https://maps.googleapis.com/maps/api/place/details/json'
+
+# Database settings
+DB_HOST = 'localhost'
+DB_NAME = 'yelp'
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+
+# Necessary businesses data
 FIELDS = (
     'name',
     'phone',
     'website',
-    'tags',
-    'addres',
+    'categories',
+    'location',
     'city',
-    'zipcode',
+    'zip_code',
     'coordinates',
     'rating',
     'google_rating',
