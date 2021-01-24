5345e07e9be4773d4bb465c492f66fcd6b098033
Artem Stepanenko <artem@stepanenko.one>
2021-01-24 22:58:44 +0200

    add try/except instruction to connection db

--- yelpme/db.py
+++ yelpme/db.py
@@ -1,6 +1,30 @@
-import pymysql
-from yelpme import sql
+from mysql.connector import Error, connect
+from yelpme import logging, settings
+
+
+class DBError(logging.YelpmeException):
+    """Database Error Exception."""
+
+    exit_code = 3
+
+    def __init__(self, message):
+        """Init Database Exception.
+
+        Args:
+            message : str
+                message error
+        """
+        self.message = message
 
 
 def save(businesses):
-    pass
+    try:
+        with connect(
+            host=settings.DB_HOST,
+            user=settings.DB_USER,
+            password=settings.DB_PASSWORD,
+        ) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute('SHOW DATABASES;')
+    except Error as err:
+        raise DBError('Can not connect to MySQL Server') from err
