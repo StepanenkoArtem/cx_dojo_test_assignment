dcb5e8039339422d28395525bf78ffb2fac07710
Artem Stepanenko <artem@stepanenko.one>
2021-01-25 09:46:57 +0200

    add xfail test

--- tests/test_api_requests.py
+++ tests/test_api_requests.py
@@ -1,15 +1,15 @@
 
+import pytest
 from yelpme import api_requests
 
 
-def test_request_yelp():
-    limit = 50
-    assert api_requests.get_yelp('vegan cafe', 'san francisco', limit)
-
-
-def test_request_gmp_id():
-    coordinates = (37.75235, -122.41925)
-    assert api_requests.get_gmp_id('Beloved Cafe', *coordinates)
+@pytest.mark.parametrize(
+    'phrase', ['', '  ', '1111']
+)
+@pytest.mark.xfail
+def test_request_yelp(phrase, location):
+    with pytest.raises(api_requests.ApiRequestError):
+        api_requests.get_yelp(phrase, 'san francisco')
 
 
 def test_request_gmp_details():
